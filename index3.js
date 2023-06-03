49bc37e40f56ec756b94e8295ab10ae728a8c382
19wh1a0579 <69663926+19wh1a0579@users.noreply.github.com>
2023-06-03 13:56:07 +0530

    Create index3.js
    DOM Practise 2

---
+++ index3.js
@@ -0,0 +1,7 @@
+var list = document.getElementsByClassName('list-group-item');
+console.log(list);
+list[2].style.backgroundColor = 'yellow';
+list[0].style.fontWeight = 'bold';
+
+var listId = document.getElementById('items');
+listId.style.fontWeight ='bold';
