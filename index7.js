e9f21d4f4595b08522c12f26c263abce45f292c1
19wh1a0579 <69663926+19wh1a0579@users.noreply.github.com>
2023-06-04 22:43:49 +0530

    Create index7.js
    DOM DELETE, EDIT AND FILTER

---
+++ index7.js
@@ -0,0 +1,62 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter')
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter Event
+filter.addEventListener('keyup', filterItems)
+//Add item
+function addItem(e){
+    e.preventDefault();
+
+
+// Get input value
+    var newItem = document.getElementById('item').value;
+
+    // Create new li element
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+
+    //Add text mode with input value
+    li.appendChild(document.createTextNode(newItem));
+    // Create delete button element
+    var deletebtn = document.createElement('button') 
+
+    deletebtn.className = 'btn btn-danger btn.sm float-right delete'
+
+    deletebtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deletebtn);
+
+    itemList.appendChild(li)
+
+}
+
+function removeItem(e){
+   if(e.target.classList.contains('delete')){
+    if(confirm('Are you sure')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+    }
+   }  
+}
+
+//Filter Items
+function filterItems(e){
+    //convert to lower case
+
+    var text = e.target.value.toLowerCase();
+    // console.log(text);
+
+    var items = itemList.getElementsByTagName('li');
+    console.log(items)
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none'
+        }
+    });
+}
